1d1a49e631b320c6228a3b2eadcc73da1cac3aa3
Fin Yang <Fin.Yang@monash.edu>
2022-10-01 01:32:42 +1000

    Special

--- R/list_fun.R
+++ R/list_fun.R
@@ -2,3 +2,10 @@
 list_fun <- list(function(a){as.list(environment())})
 
 
+#' @export
+specials_test <- fabletools::new_specials(
+  q= function(q = 1){
+    as.list(environment())
+  },
+  .required_specials = c("q")
+)
